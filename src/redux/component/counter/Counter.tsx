3ace1bfe925f29da22ee342bda4a0db27fe34934
Umer Sultan <umer.sultan@vadion.com>
2021-03-31 09:06:56 +0500

    css module added

--- src/redux/component/counter/Counter.tsx
+++ src/redux/component/counter/Counter.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useAppDispatch, useAppSelector } from '../../ReduxHooks';
 import { decrement, increment, incrementByAmount } from './counterSlice';
+import classes from './Counter.module.css';
 
 const Counter = () => {
     const count = useAppSelector(state => state.counterReducer.value);
@@ -9,7 +10,7 @@ const Counter = () => {
     //  dispatch(incrementByAmount(8))
 
     return (
-        <div>
+        <div className={classes.counter}>
             <p> counter value  {count}</p>
             <button onClick={()=>dispatch(increment())}>Increment</button>
             <button onClick={()=>dispatch(decrement())}>Decrement</button>
